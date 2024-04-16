2f3ec7e02e1c5ce2fbeb1da04418a757a1b388ce
MOKHCHY <118829186+MOKHCHY@users.noreply.github.com>
2024-04-16 10:13:15 +0100

    Update Quiz3.java

--- app/src/main/java/com/example/mokhchy_quizapp/Quiz3.java
+++ app/src/main/java/com/example/mokhchy_quizapp/Quiz3.java
@@ -15,7 +15,7 @@ public class Quiz3 extends AppCompatActivity {
     RadioButton rb;
     Button bNext;
     int score;
-    String RepCorrect="Non";
+    String RepCorrect="Oui";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
